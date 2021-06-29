01627a1ee9c8fbcd795a8a70e05fee60959c4e20
Sara Ajana <sara.ajana@kedgebs.com>
2021-06-29 14:33:59 +0200

    create a personalized message

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -77,6 +77,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $status;
 
+    public function __toString(): string
+    {
+        return $this->firstname;
+    }
+
 
     public function __construct()
     {
