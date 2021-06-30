a95277c487145707ac0c8bc0da69c6b909c2a163
P-ROGER06 <proger06@gmail.com>
2021-06-30 09:12:36 +0200

    components front

--- migrations/Version20210630063039.php
+++ migrations/Version20210630063039.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210629081400 extends AbstractMigration
+final class Version20210630063039 extends AbstractMigration
 {
     public function getDescription(): string
     {
